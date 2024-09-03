14bf2be95bd75569cea49c746682e46fbf3fa669
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-09-03 20:40:40 -0300

    improve: 2015 03/02

--- 2015/3/two.go
+++ 2015/3/two.go
@@ -9,34 +9,38 @@ func solveTwo(lines []string) {
 
 	line := lines[0]
 
+	var p string
+
 	sx, sy, rx, ry, v := 0, 0, 0, 0, make(map[string]struct{})
+
+	movements := map[rune][2]int{
+		'^': {0, -1},
+		'v': {0, 1},
+		'>': {1, 0},
+		'<': {-1, 0},
+	}
 
 	for index, m := range line {
 		rm := index % 2
-    
-		if m == '^' && rm != 0 { sy-- }
-		if m == '^' && rm == 0 { ry-- }
-		if m == 'v' && rm != 0 { sy++ }
-		if m == 'v' && rm == 0 { ry++ }
-		if m == '>' && rm != 0 { sx++ }
-		if m == '>' && rm == 0 { rx++ }
-		if m == '<' && rm != 0 { sx-- }
-		if m == '<' && rm == 0 { rx-- }
-
-		p := fmt.Sprintf("%dx%d", sx, sy)
 
 		if rm == 0 {
+			rx += movements[m][0]
+			ry += movements[m][1]
 			p = fmt.Sprintf("%dx%d", rx, ry)
+		} else {
+			sx += movements[m][0]
+			sy += movements[m][1]
+			p = fmt.Sprintf("%dx%d", sx, sy)
 		}
 
 		_, ok := v[p]
 
-    v[p] = struct{}{}
-
 		if !ok {
 			ans++
 		}
-  }
+
+		v[p] = struct{}{}
+	}
 
 	fmt.Printf("02: %d\n", ans)
 }
