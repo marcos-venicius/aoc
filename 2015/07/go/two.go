ebec7071dfdc1540e2d9f96e2d4f3705103a2e5d
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-10-26 13:48:10 -0300

    2015 07/02 GO

--- 2015/07/go/two.go
+++ 2015/07/go/two.go
@@ -7,11 +7,25 @@ import (
 )
 
 func solveTwo(reader aocreader.LinesReader) int {
-	ans := 0
+	instructions := make([]Instruction, 0)
 
 	reader.Read(func(line string) bool {
+		instruction := ParseInstruction(line)
+
+		instructions = append(instructions, instruction)
+
 		return false
 	})
+
+	solve := NewSolve(instructions)
+
+	ans := solve.Solve("a")
+
+  bValue := solve.values["a"]
+  solve.values = make(map[string]uint16)
+  solve.values["b"] = bValue
+
+	ans = solve.Solve("a")
 
 	fmt.Printf("02: %d\n", ans)
 
