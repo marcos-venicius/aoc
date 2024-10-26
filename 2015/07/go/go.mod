ee729a63e2756b5f6e3c81b165a0b1a329b136c9
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-10-26 12:59:01 -0300

    python + go day 07

---
+++ 2015/07/go/go.mod
@@ -0,0 +1,5 @@
+module aoc
+
+go 1.22.3
+
+require github.com/marcos-venicius/aocreader v0.0.0-20240906115437-f1440d6564b0 // indirect
---
+++ 2015/07/go/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/marcos-venicius/aocreader"
+
+func main() {
+	reader := aocreader.NewAocReader("./input.txt")
+
+	solveOne(reader)
+	solveTwo(reader)
+}
