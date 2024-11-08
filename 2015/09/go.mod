1d854dee302042bc27e8754bb3f8cd9d61d561e6
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-11-08 09:18:07 -0300

    2015 09/01

---
+++ 2015/09/go.mod
@@ -0,0 +1,14 @@
+module aoc
+
+go 1.22.3
+
+require (
+	github.com/marcos-venicius/aocreader v0.0.0-20240906115437-f1440d6564b0
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ 2015/09/one_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/marcos-venicius/aocreader"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGraph(t *testing.T) {
+  graph := CreateGraph()
+
+  lines := []string{
+    "London to Dublin = 464",
+    "London to Belfast = 518",
+    "Dublin to Belfast = 141",
+  }
+
+  for _, line := range lines {
+    route := ParseLine(line)
+
+    graph.Add(route)
+  }
+
+  assert.Equal(t, 3, len(graph.graph))
+  assert.Equal(t, 3, len(graph.uniqueRoutes))
+
+  assert.Equal(t, 464, graph.graph["London"]["Dublin"])
+  assert.Equal(t, 464, graph.graph["Dublin"]["London"])
+
+  assert.Equal(t, 518, graph.graph["London"]["Belfast"])
+  assert.Equal(t, 518, graph.graph["Belfast"]["London"])
+
+  assert.Equal(t, 141, graph.graph["Dublin"]["Belfast"])
+  assert.Equal(t, 141, graph.graph["Belfast"]["Dublin"])
+}
+
+func TestBaseOne(t *testing.T) {
+	reader := aocreader.NewMockReader([]string{
+    "London to Dublin = 464",
+    "London to Belfast = 518",
+    "Dublin to Belfast = 141",
+  })
+
+  ans := solveOne(reader)
+
+  assert.Equal(t, 605, ans)
+}
