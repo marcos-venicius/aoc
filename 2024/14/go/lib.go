4fa187bff5289fe5a121871ba0ce51ccd5d86113
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-12-20 20:44:14 -0300

    2024 day 14/01 go

---
+++ 2024/14/go/lib.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+)
+
+type Vec2 struct {
+	x, y int
+}
+
+type Robot struct {
+	id  int
+	pos Vec2
+	vel Vec2
+}
+
+func unwrap[T any](data T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+
+	return data
+}
+
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
+
+func (a Vec2) equal(b Vec2) bool {
+	return a.x == b.x && a.y == b.y
+}
+
+func (r *Robot) move(w, h int) {
+	r.pos.x = mod(r.pos.x+r.vel.x, w)
+	r.pos.y = mod(r.pos.y+r.vel.y, h)
+}
+
+func unique(list []int) int {
+  m := make(map[int]struct{})
+
+  for _, i := range list {
+    m[i] = struct{}{}
+  }
+
+  return len(m)
+}
+
+func getQuadrant(x, y, w, h int) Vec2 {
+	mw := w / 2
+	mh := h / 2
+
+	rx, ry := 0, 0
+
+	if x > mw {
+		rx = 1
+	}
+
+	if y > mh {
+		ry = 1
+	}
+
+	return Vec2{x: rx, y: ry}
+}
+
+func parseRobot(id int, line string) Robot {
+	regex := regexp.MustCompile(`(-?\d+)`)
+
+	matches := regex.FindAllString(line, -1)
+
+	x, y := unwrap(strconv.Atoi(matches[0])), unwrap(strconv.Atoi(matches[1]))
+	vx, vy := unwrap(strconv.Atoi(matches[2])), unwrap(strconv.Atoi(matches[3]))
+
+	return Robot{
+		id: id,
+		pos: Vec2{
+			x: x,
+			y: y,
+		},
+		vel: Vec2{
+			x: vx,
+			y: vy,
+		},
+	}
+}
---
+++ 2024/14/go/one.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/marcos-venicius/aocreader"
+)
+
+func solveOne(reader aocreader.LinesReader) int {
+	ans := -1
+
+	robots := make([]*Robot, 0)
+	robotsPerPos := make(map[Vec2][]int)
+
+	h, w := 0, 0
+
+	for reader.Running() {
+		index, line := reader.Line()
+
+		robot := parseRobot(index + 1, line)
+
+		h = max(robot.pos.y+1, h)
+		w = max(robot.pos.x+1, w)
+
+		robots = append(robots, &robot)
+	}
+
+  var wg sync.WaitGroup
+
+  for _, robot := range robots {
+    wg.Add(1)
+
+    go func () {
+      defer wg.Done()
+
+      for i := 0; i < 100; i++ {
+        robot.move(w, h)
+      }
+    }()
+  }
+
+  wg.Wait()
+
+  for _, robot := range robots {
+    if _, ok := robotsPerPos[robot.pos]; !ok {
+      robotsPerPos[robot.pos] = make([]int, 0)
+    }
+
+    robotsPerPos[robot.pos] = append(robotsPerPos[robot.pos], robot.id)
+  }
+
+  quadrants := make(map[Vec2]int)
+
+	for y := 0; y < h; y++ {
+    if y == h / 2 {
+      continue
+    }
+
+		for x := 0; x < w; x++ {
+      if x == w / 2 {
+        continue
+      }
+
+			if c, ok := robotsPerPos[Vec2{x: x, y: y}]; ok {
+        quadrant := getQuadrant(x, y, w, h)
+
+        quadrants[quadrant] += unique(c)
+      }
+		}
+	}
+
+  for _, v := range quadrants {
+    if ans == -1 {
+      ans = v
+    } else {
+      ans *= v
+    }
+  }
+
+	fmt.Printf("01: %d\n", ans)
+
+	return ans
+}
