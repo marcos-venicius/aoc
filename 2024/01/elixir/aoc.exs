822951a7290cfdf3036f41001e8925382fa5d1a6
Marcos Venicius <marcosdevelopment.business@gmail.com>
2024-12-25 12:16:20 -0300

    2024 day 01/02 Elixir

--- 2024/01/elixir/aoc.exs
+++ 2024/01/elixir/aoc.exs
@@ -1,4 +1,6 @@
 defmodule Aoc do
+  @input_file "input.txt"
+
   def extract_digits(string) do
     string
     |> String.split("\n", trim: true)
@@ -15,33 +17,41 @@ defmodule Aoc do
       end
     end)
     |> handle_result()
-    |> diff()
-    |> sum()
   end
 
   defp handle_result({:error, _} = error), do: error
 
   defp handle_result({left_digits, right_digits}) do
-    {:ok, Enum.sort(left_digits), Enum.sort(right_digits)}
+    {Enum.sort(left_digits), Enum.sort(right_digits)}
   end
 
   defp diff({:error, _} = error), do: error
 
-  defp diff({:ok, left_digits, right_digits}) do
-    {:ok,
-     Enum.zip(left_digits, right_digits)
-     |> Enum.map(fn {left, right} -> abs(right - left) end)}
+  defp diff({left_digits, right_digits}) do
+    Enum.zip(left_digits, right_digits)
+    |> Enum.map(fn {left, right} -> abs(right - left) end)
   end
 
   defp sum({:error, _} = error), do: error
-  defp sum({:ok, []}), do: 0
-  defp sum({:ok, [head | tail]}), do: head + sum({:ok, tail})
+  defp sum([]), do: 0
+  defp sum([head | tail]), do: head + sum(tail)
+
+  defp calc_score({:error, _} = error), do: error
+
+  defp calc_score({[], _}), do: 0
+
+  defp calc_score({[head | left_digits], right_digits}) do
+    head * Enum.count(right_digits, fn x -> x == head end) +
+      calc_score({left_digits, right_digits})
+  end
 
   def part_one() do
-    case File.read("input.txt") do
+    case File.read(@input_file) do
       {:ok, content} ->
         content
-        |> extract_digits
+        |> extract_digits()
+        |> diff()
+        |> sum()
         |> case do
           {:error, message} -> IO.puts("Could not run part one due to: #{message}")
           ans -> IO.puts("Part 01: #{ans}")
@@ -51,6 +61,23 @@ defmodule Aoc do
         IO.puts("Failed to read input due to: #{reason}")
     end
   end
+
+  def part_two() do
+    case File.read(@input_file) do
+      {:ok, content} ->
+        content
+        |> extract_digits()
+        |> calc_score()
+        |> case do
+          {:error, message} -> IO.puts("Could not run part two due to: #{message}")
+          ans -> IO.puts("Part 02: #{ans}")
+        end
+
+      {:error, reason} ->
+        IO.puts("Failed to read input due to: #{reason}")
+    end
+  end
 end
 
 Aoc.part_one()
+Aoc.part_two()
