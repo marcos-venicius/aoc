238b0ee2ad834c2296d39af4f427a719e98cc6bd
Marcos <marcosdevelopment.business@gmail.com>
2026-05-31 14:53:59 -0300

    aoc(2019, day: 01, part: 02, lang: python)

--- 2019/01/python/main.py
+++ 2019/01/python/main.py
@@ -9,3 +9,18 @@ data = re.split(r'\n|\s', open(file, 'r').read())
 part_one = sum([int(x) // 3 - 2 for x in data if x != ''])
 
 print(f'P1: {part_one}')
+
+def calc_fuel(of):
+    result = 0
+    of = of // 3 - 2
+
+    while of > 0:
+        result += of
+
+        of = of // 3 - 2
+
+    return result
+
+part_two = sum([calc_fuel(int(x)) for x in data if x != ''])
+
+print(f'P2: {part_two}')
