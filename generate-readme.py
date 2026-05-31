c0db0ed7f43b9b6db83e7ec235a6fa1de3898288
Marcos <marcosdevelopment.business@gmail.com>
2026-05-31 13:54:16 -0300

    update readme

--- generate-readme.py
+++ generate-readme.py
@@ -16,26 +16,48 @@ readme = [
 ]
 
 exclude_files = set(['sum', 'mod', 'txt', 'md', 'gitignore'])
+exclude_dirs = set(['assets'])
 years = sorted([x for x in os.listdir('.') if x.isnumeric() and os.path.isdir(x)], key=lambda x: int(x), reverse=True)
+langs_map = {
+  'c': 'C',
+  'c3': 'C3',
+  'py': 'Python',
+  'python': 'Python',
+  'odin': 'Odin',
+  'go': 'Go',
+  'elixir': 'Elixir',
+  'zig': 'Zig'
+}
+
+def map_lang(lang):
+  if lang not in langs_map:
+    return lang
+  return langs_map[lang]
+
+def map_aoc_year(year):
+  return f'https://adventofcode.com/{year}'
+
+def map_aoc_day(year, day):
+  return f'https://adventofcode.com/{year}/day/{day}'
 
 for year in years:
-    readme.append(f'- [**{year}**](./{year})')
+    readme.append(f'- [{year}](./{year}) [↗AOC]({map_aoc_year(year)})')
 
     days = sorted(os.listdir(os.path.join('./', year)), key=lambda x: int(x), reverse=True)
 
     for day in days:
         entries = sorted([x for x in os.listdir(os.path.join('./', year, day))])
-        dirs = [x for x in entries if os.path.isdir(os.path.join('./', year, day, x))]
+        dirs = [x for x in entries if os.path.isdir(os.path.join('./', year, day, x)) if x not in exclude_dirs]
         files = [x for x in entries if os.path.isfile(os.path.join('./', year, day, x))]
 
         if len(dirs) > 0:
-            readme.append(f'  - [**Day {day}**]({os.path.join("./", year, day)})')
+            readme.append(f'  - [{day}]({os.path.join("./", year, day)}) [↗AOC]({map_aoc_day(year, day)})')
             for d in dirs:
-                readme.append(f'    - [**{d}**]({os.path.join("./", year, day, d)})')
+                readme.append(f'    - [{map_lang(d)}]({os.path.join("./", year, day, d)})')
         else:
-            langs = ', '.join([x for x in set([x.split('.')[-1] for x in files]) if x not in exclude_files])
+            langs = ', '.join([map_lang(x) for x in set([x.split('.')[-1] for x in files]) if x not in exclude_files])
 
-            readme.append(f'  - [**Day {day}** ({langs})]({os.path.join("./", year, day)})')
+            readme.append(f'  - [{day} ({langs})]({os.path.join("./", year, day)})')
         
 readme.append("")
 
