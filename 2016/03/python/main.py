a121a0d2a66ed468c4e76a46b601cf08dbc0539b
Marcos <marcosdevelopment.business@gmail.com>
2026-05-31 16:27:41 -0300

    aoc(2016, day: 03, part: 02, lang: python)

--- 2016/03/python/main.py
+++ 2016/03/python/main.py
@@ -12,3 +12,16 @@ part_one = len([t for t in data if sum(sorted(t)[:-1]) > max(t)])
 
 print(f'P1: {part_one}')
 
+vertical = [[], [], []]
+
+for t in data:
+  vertical[0].append(t[0])
+  vertical[1].append(t[1])
+  vertical[2].append(t[2])
+
+data = vertical[0] + vertical[1] + vertical[2]
+data = [data[i:i+3] for i in range(0, len(data), 3)]
+
+part_two = len([t for t in data if sum(sorted(t)[:-1]) > max(t)])
+
+print(f'P2: {part_two}')
