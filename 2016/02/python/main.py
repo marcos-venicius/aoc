3de0003b8faa20a0eb517aa8127ea63d4df23ecd
Marcos <marcosdevelopment.business@gmail.com>
2026-05-31 14:21:06 -0300

    aoc(2016, day: 02, part: 01, lang: python)

--- 2016/02/python/main.py
+++ 2016/02/python/main.py
@@ -4,37 +4,38 @@ file = './input.txt'
 
 instructions = [line.split('\n')[0] for line in open(file, 'r').readlines()]
 
-keypad = [
+class PadController:
+    def __init__(self, keypad, start=(0, 0)):
+        self.x = start[0]
+        self.y = start[1]
+        self.keypad = keypad
+        self.M = { 'U': (0, -1), 'R': (1, 0), 'D': (0, 1), 'L': (-1, 0) }
+
+    def move(self, movement):
+        m = self.M[movement]
+
+        nx = self.x + m[0]
+        ny = self.y + m[1]
+
+        if ny >= 0 and ny < len(self.keypad) and nx >= 0 and nx < len(self.keypad[ny]):
+            self.x = nx
+            self.y = ny
+
+    def exec_instruction(self, instructions):
+        code = []
+
+        for instruction_set in instructions:
+            for movement in instruction_set:
+                self.move(movement)
+
+            code.append(self.keypad[self.y][self.x])
+
+        return ''.join(map(str, code))
+
+part_one = PadController([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
-]
+], (1, 1))
 
-# starts at button "5"
-x = 1
-y = 1
-M = { 'U': (0, -1), 'R': (1, 0), 'D': (0, 1), 'L': (-1, 0) }
-
-def move(movement):
-    global x, y
-
-    m = M[movement]
-
-    nx = x + m[0]
-    ny = y + m[1]
-
-    if ny >= 0 and ny < len(keypad) and nx >= 0 and nx < len(keypad[ny]):
-        x = nx
-        y = ny
-
-code = []
-
-for instruction_set in instructions:
-    for movement in instruction_set:
-        move(movement)
-
-    code.append(keypad[y][x])
-
-part_one = ''.join(map(str, code))
-
-print(f'P1: {part_one}')
+print(f'P1: {part_one.exec_instruction(instructions)}')
