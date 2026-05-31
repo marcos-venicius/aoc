4de86359881d8947a5795e3c22c2e4b2b3b202d4
Marcos <marcosdevelopment.business@gmail.com>
2026-05-31 14:44:07 -0300

    aoc(2018, day: 01, part: 02, lang: python)

--- 2018/01/python/main.py
+++ 2018/01/python/main.py
@@ -4,7 +4,7 @@ import re
 
 file = './input.txt'
 
-data = [x for x in re.split(r'\n|\s', open(file, 'r').read()) if x != '']
+data = [x for x in re.split(r'\n|\s|,', open(file, 'r').read()) if x != '']
 
 part_one = 0
 
@@ -17,3 +17,24 @@ for freq in data:
         raise Exception(f"invalid {freq}")
 
 print(f'P1: {part_one}')
+
+acc = 0
+seen = set()
+part_two = 0
+
+while part_two == 0:
+    for freq in data:
+        if freq[0] == '+':
+            acc += int(freq[1:])
+        elif freq[0] == '-':
+            acc -= int(freq[1:])
+        else:
+            raise Exception(f"invalid {freq}")
+
+        if acc in seen:
+            part_two = acc
+            break
+
+        seen.add(acc)
+
+print(f'P2: {part_two}')
