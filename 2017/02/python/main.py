8b82a176b46539e0279144ad3f0ddcad07129a58
Marcos <marcosdevelopment.business@gmail.com>
2026-06-01 13:24:55 -0300

    aoc(2017, day: 02, part: 02, lang: python)

--- 2017/02/python/main.py
+++ 2017/02/python/main.py
@@ -9,3 +9,20 @@ data = [list(map(int, re.split(r'\s', x))) for x in open(file, 'r').read().split
 part_one = sum([max(x) - min(x) for x in data])
 
 print(f'P1: {part_one}')
+
+def get_divisors(line):
+    for i in range(len(line)):
+        for j in range(len(line)):
+            if i == j: continue
+
+            a = line[i]
+            b = line[j]
+
+            if a / b % 1 == 0:
+                return a // b
+
+    return 0
+
+part_two = sum([get_divisors(x) for x in data])
+
+print(f'P2: {part_two}')
